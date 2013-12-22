67b9acf704f1f6a35d1827bda4f6745831d2e070
vishnun <vishnunarang2000@gmail.com>
2013-12-23 01:03:54 +0530

    Vishnu: Home page now opens modal if word clicked

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,41 @@
-$(document).ready(function(){
-   $("button[data-target-modal=rootword-modal]").on('click', function(event){
-      var rootword = $(this).children('.rootword-data').data('rootword');
+$(document).ready(function () {
 
-      var modal = $('#rootword-modal');
-      modal.find("[data-rootword='word']").text(rootword.word);
-      modal.find("[data-rootword='meaning']").text(rootword.meaning);
-      modal.find("[data-rootword='usages']").empty().append("<li> <h3>"+ rootword.usages +"</h3></li>");
+    var bindRootwordModal = function () {
+        $("button[data-target-modal=rootword-modal]").on('click', function (event) {
+            var rootword = $(this).find('.rootword-data').data('rootword');
 
-      event.preventDefault();
-   });
+            var modal = $('#rootword-modal');
+            modal.find("[data-rootword='word']").text(rootword.word);
+            modal.find("[data-rootword='meaning']").text(rootword.meaning);
+            modal.find("[data-rootword='usages']").empty().append("<li>" + rootword.usages + "</li>");
+
+            event.preventDefault();
+        });
+    };
+
+
+    var bindNonRootwordModal = function () {
+        $("button[data-target-modal=non-rootword-modal]").on('click', function (event) {
+            var nonRootword = $(this).find('.non-rootword-data').data('non-rootword');
+            nonRootword.rootword = $('#'+nonRootword.word).find('.rootword-data').data('rootword');
+
+            var modal = $('#non-rootword-modal');
+            modal.find("[data-non-rootword='word']").text(nonRootword.word);
+            modal.find("[data-non-rootword='meaning']").text(nonRootword.meaning);
+            var usagesEl = modal.find("[data-non-rootword='usages']");
+            usagesEl.empty();
+            for(var ind = 0; ind < nonRootword.usages.length; ind++) {
+                usagesEl.append("<li>" + nonRootword.usages[ind] + "</li>");
+            }
+            modal.find("[data-non-rootword='rootword-data']").data('rootword', nonRootword.rootword);
+            modal.find("[data-non-rootword='rootword']").text(nonRootword.rootword.word);
+
+            event.preventDefault();
+        });
+    };
+
+    bindRootwordModal();
+    bindNonRootwordModal();
 });
+
+

8cf5dae58a7b28bbc7abd19c25ddc4fe744c469f
vishnun <vishnunarang2000@gmail.com>
2013-12-22 15:06:18 +0530

    Vishnu : Add spec for rootwords

--- spec/models/rootword_spec.rb
+++ spec/models/rootword_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Rootword do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'serialization' do
+    it { should serialize :usages }
+  end
 end

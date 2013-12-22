9d09c1e8c30511861d4ba13e56a345e21c3874b3
vishnun <vishnunarang2000@gmail.com>
2013-12-22 11:11:19 +0530

    [#0] Vishnu: Setup initial project with devise gem for authentication for admin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etymology::Application.config.secret_key_base = 'e98168176800b8d279f325e4262bd9da752436eb5e3369a42b31aa6f39a3fc68b338d38fd3b673b5c5be7bf013bafadc0159e5f700acf24388dadac5a5b4a2a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Etymology::Application.config.session_store :cookie_store, key: '_etymology_session'
